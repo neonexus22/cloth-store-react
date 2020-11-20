3d871eec24877c7e03381e8481379fecf7107bbb
NishantShah <nishant@genesesolution.com>
2020-11-20 15:03:47 +0545

    updated shop data (new data), cart utils added and logic added

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 import "./custom-button.styles.css";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
   <button
-    className={`${isGoogleSignIn ? "google-sing-in" : ""} custom-button`}
+    className={`${isGoogleSignIn ? "google-sing-in" : ""} ${
+      inverted ? "inverted" : ""
+    } custom-button`}
     {...otherProps}
   >
     {children}
